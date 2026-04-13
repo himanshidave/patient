9716cb83529a840a334fb98ec486ebd03004201b
Himanshi Dave <himanshidave03mar@gmail.com>
2026-04-13 18:28:36 +0530

    sorting data using Query

--- main.py
+++ main.py
@@ -1,35 +1,66 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
-
-app=FastAPI()
+app = FastAPI()
 
 def data_load():
-    with open("patients.json","r") as f:
-        data = json.load(f)
-
-    return data
+    with open("patients.json", "r") as f:
+        return json.load(f)
 
 @app.get("/")
 def home():
-    return{"msg":"Patient management system"}
+    return {"msg": "Patient management system"}
 
 @app.get('/about')
 def about():
-    return{"msg":"API to manage patients records"}
-
+    return {"msg": "API to manage patients records"}
 
 @app.get("/view")
 def view():
-    data = data_load()
-    return data 
+    return data_load()
 
 @app.get("/patient/{patient_id}")
-def view_patient(patient_id: str):
+def view_patient(
+    patient_id: str = Path(..., description="Patient id in DB", example="P002")
+):
     data = data_load()
 
     for patient in data:
         if patient["id"] == patient_id:
             return patient
     
-    return("Id doesnt exist")
+    raise HTTPException(status_code=404 ,detail="Patient not found")
+
+@app.get("/sort")
+def sort_patients(
+    sort_by: str = Query(..., description="Sort by field (height, weight, bmi)"),
+    order: str = Query("asc", description="Sort order: asc or desc")
+):
+    data = data_load()
+
+    valid_fields = ["height", "weight", "bmi"]
+
+    # validate field
+    if sort_by not in valid_fields:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Invalid field. Choose from {valid_fields}"
+        )
+
+    # validate order
+    if order not in ["asc", "desc"]:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid order. Use 'asc' or 'desc'"
+        )
+
+    # sorting logic
+    reverse = True if order == "desc" else False
+
+    sorted_data = sorted(
+        data,
+        key=lambda x: x.get(sort_by, 0),
+        reverse=reverse
+    )
+
+    return sorted_data
